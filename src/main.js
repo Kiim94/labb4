37b088b3491c45025ab41cd7cf5aa346030ac1c5
Kiim94 <nemomoa@hotmail.com>
2026-02-16 15:22:38 +0100

    Lite javascript för smiley, bild wobble

--- src/main.js
+++ src/main.js
@@ -14,11 +14,16 @@ const originalColor = ouch.style.color;
 
 
 smiley.addEventListener("click", function() {
+    smiley.classList.remove("wobble");
     if (smiley.innerHTML === "😊"){
         smiley.classList.add("shake");
         smiley.innerHTML = "🤕";
         ouch.innerHTML = "Ow! Hur kunde du?!";
         ouch.style.color="red";
+    } else if(smiley.innerHTML === "🤕"){
+        smiley.classList.add("wobble");
+        smiley.innerHTML = "💀";
+        ouch.innerHTML = ". . .";
     } else {
         smiley.innerHTML = "😊";
         ouch.innerHTML = "Klicka på smileyn";
