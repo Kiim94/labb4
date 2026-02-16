1fee5acbcf0df782b53c027e1f4439268f4c4ca2
Kiim94 <nemomoa@hotmail.com>
2026-02-16 14:02:40 +0100

    Mer animationer, klickbara och laddnings-snurra

--- src/main.js
+++ src/main.js
@@ -6,3 +6,23 @@ const menu = document.querySelector("nav ul.menu");
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+//skadad smiley vid tryck
+const smiley = document.querySelector("#smiley");
+const ouch = document.querySelector("#ouch");
+const originalColor = ouch.style.color;
+
+
+smiley.addEventListener("click", function() {
+    if (smiley.innerHTML === "😊"){
+        smiley.classList.add("shake");
+        smiley.innerHTML = "🤕";
+        ouch.innerHTML = "Ow! Hur kunde du?!";
+        ouch.style.color="red";
+    } else {
+        smiley.innerHTML = "😊";
+        ouch.innerHTML = "Klicka på smileyn";
+        smiley.classList.remove("shake");
+        ouch.style.color=originalColor;
+    }
+});
