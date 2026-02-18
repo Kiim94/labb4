97622e2534c2e963e2ac3947e3a76817cefc94a0
Kiim94 <nemomoa@hotmail.com>
2026-02-18 17:41:30 +0100

    Gjorde studs på mås-text, flyttat alla partials till egen mapp, skapat specifik scss fil för animationer

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
     //skriv vilket plugin som används
-    base: "/labb3-frontend/",
+    base: "/labb4/",
     plugins: [react(),
         ViteImageOptimizer({
             png: {quality:80},
